51b6283501aa345aa82bf6de5632fa54a554aec3
Joaquin1128 <Joaquin.augusto@hotmail.com>
2025-10-26 18:52:08 -0300

    base para el back

---
+++ laser-cut-back/src/main/java/com/example/lasercut/laser_cut_back/LaserCutBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.lasercut.laser_cut_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaserCutBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaserCutBackApplication.class, args);
+	}
+
+}
---
+++ laser-cut-back/src/test/java/com/example/lasercut/laser_cut_back/LaserCutBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lasercut.laser_cut_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaserCutBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
